8147c51c8531f57f30a6edf479a2c46fc0253ea9
bojidaryovchev <blizzardcon10@gmail.com>
2026-04-04 21:02:13 +0300

    feat: electron app init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'system-ui, sans-serif' }}>
+      <h1>ReqResFlow</h1>
+      <p>Your API client is ready. Start building!</p>
+    </div>
+  );
+};
+
+export default App;
